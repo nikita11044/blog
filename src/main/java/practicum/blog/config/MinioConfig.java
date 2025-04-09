30790a9e75f6ef3d704a3cfbb3e99cb5df42c2e8
iskorostinskiinikita <nikita11042000@gmail.com>
2025-04-09 23:49:01 +0300

    feat: added minio

---
+++ src/main/java/practicum/blog/config/MinioConfig.java
@@ -0,0 +1,29 @@
+package practicum.blog.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "minio")
+public class MinioConfig {
+
+    @Value("${blog.file-storage.endpoint}")
+    private String url;
+
+    @Value("${blog.file-storage.user}")
+    private String accessKey;
+
+    @Value("${blog.file-storage.password}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
